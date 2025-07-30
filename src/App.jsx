99125456819ce802494b7d77ff3d0abe01a2efc5
ABHINAV <2300031133@gmail.com>
2025-07-30 10:17:32 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './style.CSS';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - S105</h1>
+      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+    </div>
+  )
+}
+
+export default App
